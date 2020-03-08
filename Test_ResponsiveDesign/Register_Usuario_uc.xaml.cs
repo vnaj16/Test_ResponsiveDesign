13379ad1f826c06dd7d489c9d0adfb3328196122
Arthur <arthur1610@live.com>
2020-03-08 17:14:07 -0500

    Initial commit

---
+++ Test_ResponsiveDesign/Register_Usuario_uc.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Test_ResponsiveDesign
+{
+    /// <summary>
+    /// Interaction logic for Register_Usuario_uc.xaml
+    /// </summary>
+    public partial class Register_Usuario_uc : UserControl
+    {
+        public Register_Usuario_uc()
+        {
+            InitializeComponent();
+        }
+    }
+}
